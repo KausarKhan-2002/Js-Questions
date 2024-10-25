bc1faceef7264b5a7718230b97d515c2af6f1892
Kausar Khan <kausarrkhan83@gmail.com>
2024-10-25 10:31:03 +0530

    Only JS Questions

---
+++ app.js
@@ -0,0 +1,32 @@
+// Array ___________________________
+// Q.1 WAP and return sum of all the elements from array ?
+// Q.2 WAP to count index of array if index is 0 return -1
+// Q.3 WAP and reverse an array ?
+// Q.4 WAP and findout k from array if found return index if not found return -1 ?
+// Q.5 WAP to get all the even numbers in another array ?
+// Q.6 WAP and findout all vowels from array if vowels is not return null, assume dublicate
+//     vowel is not in array ?
+// Q.7 WAP and return multiple of 1st, middle and last element from array ?
+// Q.8 WAP and check your name exists in array or not if exist return index otherwise return -1 ?
+// Q.9 WAP and take your name in string and match your name in array using random index if name
+//     match return winner otherwise looser, your name must be in array as well as random name ?
+
+
+// String ___________________________
+// Q.1 WAP to get a string in reverse order ?
+// Q.2 WAP to get your name devided in 2 parts if included title then title should be consider
+//     seprately exluded your exact name?
+// Q.3 WAP to get month using number if number does not match print invalid month ?
+// Q.4 WAP convert a string into array every character should be seprate in array ?
+// Q.5 WAP and take prompt if user write his name then print hello + username if user do not
+//     write anything, keep showing prompt untill user do not write atleast 1 character ?
+
+
+// Number ___________________________
+// Q.1 WAP and take a number if number is 2 digits sum number from each other if number is more
+//     then 2 digits take all the digits instead of last digit and then sum from last digit ?
+// Q.2 WAP and check n is equal to the length of array or not if n is equal to the length of
+//     array then return true otherwise append greatest element from last untill length of array
+//     becomes equal to n ?
+// For EX : [10,20,30], n = 3  output => true
+// [10,20,30], n = 7  output => [10,20,30, d > 30, d1 > d, d2 ? d1, d3 > d2]
